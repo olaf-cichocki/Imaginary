07e28b0d51d83d5f04e811d43eb00f8c92b6257e
Olaf Cichocki <olaf.cichocki@gmail.com>
2019-11-17 21:32:04 +0100

    Try other CI config

--- mix.exs
+++ mix.exs
@@ -14,10 +14,6 @@ defmodule Imaginary.MixProject do
       package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [
-        main: "readme",
-        extras: ["README.md", "CHANGELOG.md"]
-      ],
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -26,7 +22,11 @@ defmodule Imaginary.MixProject do
         "coveralls.html": :test
       ],
       dialyzer: [
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+        plt_add_apps: [:mix, :ex_unit]
+      ],
+      docs: [
+        main: "readme",
+        extras: ["README.md", "CHANGELOG.md"]
       ]
     ]
   end
@@ -56,11 +56,11 @@ defmodule Imaginary.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false},
       {:junit_formatter, "~> 2.1", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
-      {:credo, "~> 0.8", only: [:dev, :test]},
-      {:excoveralls, "~> 0.8", only: :test}
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false},
+      # {:credo, "~> 0.8", only: [:dev, :test]},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:earmark, "~> 1.4", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
