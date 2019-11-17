e1b890190b6f5af85ff40d9e7486c96290112b83
Olaf Cichocki <olaf.cichocki@gmail.com>
2019-11-17 22:20:48 +0100

    Fix coverage report generation

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Imaginary.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.json": :test
       ],
       dialyzer: [
         plt_add_apps: [:mix, :ex_unit]
@@ -58,9 +58,9 @@ defmodule Imaginary.MixProject do
     [
       {:junit_formatter, "~> 2.1", only: :test},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false},
-      # {:credo, "~> 0.8", only: [:dev, :test]},
+      {:credo, "~> 0.9", only: [:dev, :test]},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.12", only: :test}
       # {:earmark, "~> 1.4", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
